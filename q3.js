be95b49d082056746c7bfbf8dd4e1f0df9c97b44
shruthi-reddy14 <amireddyshruthi14@gmail.com>
2025-05-29 18:48:59 +0530

    updated

---
+++ q3.js
@@ -0,0 +1,4 @@
+db.products.insertMany([
+    {name:"gem",description:"Candie",price:40,img:"https://picsum.photos/200/300"},
+{name:"chocolate",description:"dessert",price:30,img:"https://picsum.photos/200/300"},
+{name:"Biryani",description:"Main Course",price:250,img:"https://picsum.photos/200/300"}])
